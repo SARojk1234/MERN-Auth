6e8ef07572565de1d0d8590e75f01d4c80e70c33
SARojk1234 <sarojking720@gmail.com>
2024-03-11 19:13:21 +0530

    create api route for update

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,9 +3,11 @@ import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
 import {app} from '../firebase'
 import {useDispatch} from 'react-redux';
 import {signInSuccess} from '../redux/user/userSlice'
+import { useNavigate} from 'react-router-dom'
 
 const OAuth = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate()
 const handleGoogleClick =async()=>{
     
     try{
@@ -24,7 +26,11 @@ const handleGoogleClick =async()=>{
             })
         })
         const data = await res.json()
-        dispatch(signInSuccess(data))
+        if(res.ok){
+            dispatch(signInSuccess(data))
+            navigate('/')
+        }
+       
 
     }catch(error){
         console.log('could not login with google', error);
